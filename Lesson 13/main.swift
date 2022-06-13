19bdd7fc2dab39f13a0f41fe0c19e590005f3c81
7Sergey7 <stepanyansergey7@gmail.com>
2022-06-13 23:43:33 +0400

    Initial Commit

---
+++ Lesson 13/main.swift
@@ -0,0 +1,142 @@
+//
+//  main.swift
+//  Lesson 13
+//
+//  Created by Seroj on 04.06.22.
+//
+
+import Foundation
+
+//class User {
+//
+//}
+
+//struct User {
+//
+//}
+
+//let a: (String, Int) = ("", 23)
+//
+//struct Person {
+//    let name: String = ""
+//    var age: Int = 23
+//}
+//
+//let man: Person = Person()
+//print(man.name)
+
+
+//struct User {
+//    var name: String = ""
+//    var phoneNumber: String = ""
+//    var age: Int = 0
+//}
+//
+//var personOne: User = User()
+//
+//print(personOne.name)
+//print(personOne.phoneNumber)
+//print(personOne.age)
+//
+//personOne.name = "Anna"
+//personOne.phoneNumber = "077787878"
+//personOne.age = 23
+//
+//print(personOne.name)
+//print(personOne.phoneNumber)
+//print(personOne.age)
+
+//
+//class User {
+//    var name: String = ""
+//    var phoneNumber: String = ""
+//    var age: Int = 0
+//}
+//
+//let personOne: User = User()  //////// toxuma letov poxenq vortev referens type a
+//
+//personOne.name = "Anna"
+//personOne.phoneNumber = "077787878"
+//personOne.age = 23
+//
+//print(personOne.name)
+//print(personOne.phoneNumber)
+//print(personOne.age)
+//
+//
+//
+//let person2 = User()
+//
+//personOne.name = "Alla"
+//personOne.phoneNumber = "077000000"
+//personOne.age = 23
+
+///////personOne = person2    /// hasecen chi kara poxi qani vor let a personOne ete poxenq var karanq
+
+
+//enum CarType {
+//    case sedan
+//    case jeep
+//}
+//
+//struct Car {
+//    var type: CarType = CarType.sedan
+//    var marka: String = ""
+//    var model: String = ""
+//    var color: String = ""
+//
+//}
+//
+//var car:Car = Car()
+//car.marka = "BMW"
+//car.model = "3"
+//car.color = "Silver"
+//car.type = .jeep
+//print(car)
+
+
+
+
+//class User {
+//    var name: String = ""
+//    var phoneNumber: String = ""
+//    var age: Int = 0
+//}
+//
+//var personOne: User = User()
+//
+//personOne.name = "Anna"
+//personOne.phoneNumber = "077787878"
+//personOne.age = 23
+//
+//print(personOne.name)
+//print(personOne.phoneNumber)
+//print(personOne.age)
+//
+//let person2 = User()
+//
+//personOne.name = "Alla"
+//personOne.phoneNumber = "077000000"
+//personOne.age = 23
+//
+//personOne = person2
+//
+//if personOne === person2 {
+//    print("nuyn texn en hxvum ozuyum")
+//} else if personOne !== person2 {
+//    print("nuyn texy chen hxvum ozuyum")
+//}
+
+
+
+//struct User {
+//    let name: String
+//    let phone: String
+//    let age: Int
+//}
+//
+//var person = User(name: "Anna", phone: "098777776", age: 23)
+//person = User(name: "d", phone: "3", age: 24)
+//
+//print(person)
+
